8dd911a36e9a19a0a586572895809d15e54192d8
erkelost <1256029807@qq.com>
2021-11-12 12:24:45 +0800

    feat: 'ts'

--- build/context/index.es.js
+++
@@ -1,9 +0,0 @@
-import { reactive } from "vue";
-const context = {
-  locks: {},
-  zIndex: 2e3,
-  touchmoveForbid: true
-};
-const _ContextComponent = reactive(context);
-var index = reactive(context);
-export { _ContextComponent, index as default };
